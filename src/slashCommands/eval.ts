a2d5ebffbc4dd4db954ef7757e7907c47bdb5ac2
lumap <lumapbk@gmail.com>
2022-03-20 13:27:58 +0100

    fix - you can now use _client in eval command

--- src/slashCommands/eval.ts
+++ src/slashCommands/eval.ts
@@ -1,6 +1,7 @@
 import { CommandInteraction } from "discord.js";
+import BotClient from "../classes/Client";
 
-export async function execute(interaction: CommandInteraction) {
+export async function execute(interaction: CommandInteraction, _client: BotClient) {
     await interaction.deferReply();
     let ev = interaction.options.getString("code") || ""; //@ts-ignore
     try { ev = eval(ev) } catch (e) { ev = e };
