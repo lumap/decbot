d616c47a7581b662d3891208b59ad080da91d799
serotoninx <82023017+serotoninx@users.noreply.github.com>
2022-03-24 12:43:54 +0800

    Update cause i forgot the semi colons

--- src/functions/getWord.ts
+++ src/functions/getWord.ts
@@ -1,11 +1,11 @@
-import { possibleWordleSolutions } from '../constants/wordle.ts'
+import { possibleWordleSolutions } from '../constants/wordle.ts';
 
 export function getDaily(){
-    var DT = new Date()
+    var DT = new Date();
     const random = Math.floor(Math.random() * possibleWordleSolutions.length);
 
-    if(DT.getUTCHours == 1){
-        client.db.set('word',possibleWordleSolutions[random])
+    if(DT.getUTCHours < 1){
+        client.db.set('word',possibleWordleSolutions[random]);
     }
-    return client.db.get('word')
+    return client.db.get('word');
 }
